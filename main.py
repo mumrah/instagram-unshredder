73730bee9fb5db16b4b11f7553c45a5e3200930d
David Arthur <darthur@digitalsmiths.com>
2011-11-16 11:37:19 -0500

    Changed method of adding slices
    Also stole a distance function from
    http://chaoxuprime.com/2011/11/a-solution-to-Instagram-engineering-challenge-the-unshredder-in-haskell

--- main.py
+++ main.py
@@ -7,87 +7,70 @@ def get_col(data, w, h, i):
         col.append(data[j*w + i])
     return col
 
-def px_to_lum(r, g, b, _):
-    return 0.2126*r + 0.7152*g + 0.0722*b
-
 def dist(a, b):
+    # Borrowed from
+    # http://chaoxuprime.com/2011/11/a-solution-to-Instagram-engineering-challenge-the-unshredder-in-haskell
     d = 0.
     for x, y in zip(a, b):
-        d += (x-y)**2
-    return d**0.5
+        d += (abs(x[0]-y[0]) + abs(x[1]-y[1]) + abs(x[2]-y[2]))**0.25
+    return d
+
+def unshred(im, ordering):
+    unshredded = Image.new("RGBA", im.size)
+    for i, si in enumerate(ordering):
+        x1, y1 = 32*si, 0
+        x2, y2 = x1 + 32, h
+        slice_im = im.crop((x1, y1, x2, y2))
+        unshredded.paste(slice_im, (i*32, 0))
+    unshredded.show()
 
 if __name__ == "__main__":
     im = Image.open("TokyoPanoramaShredded.png", "r")
-    im.show()
-    print im.size
+    # convert to XYZ
+    rgb2xyz = (
+            0.412453, 0.357580, 0.180423, 0,
+            0.212671, 0.715160, 0.072169, 0,
+            0.019334, 0.119193, 0.950227, 0 )
+    im1 = im.convert("RGB")
+    im2 = im1.convert("RGB", rgb2xyz)
+
     w, h = im.size
-    data = im.getdata()
-    slices = []
+    data = im2.getdata()
+    slices= []
 
     # Extract the slice edges
     for i in range(w/32):
         left = get_col(data, w, h, i*32)
         right = get_col(data, w, h, (i+1)*32-1)
-        slices.append((left, right))
+        slices.append((i, left, right))
 
-    # Calculate luminances
-    slice_lums = []
-    for left, right in slices:
-        slice_lums.append((
-            [px_to_lum(*px) for px in left],
-            [px_to_lum(*px) for px in right]
-        ))
-
-
-    lefts = {}
-    rights = {}
-    for s in range(len(slice_lums)):
-        this_slice = slice_lums[s]
+    # Iteratively add slices to the output
+    sl = slices.pop(0)
+    ordering = [sl[0]]
+    left_end = sl[1]
+    right_end = sl[2]
+    while len(slices) > 0:
+        # left slices sorted by distance
         left_slices = sorted(
-            [(i, dist(this_slice[0], slice_lums[i][1]))
-                for i in range(len(slice_lums)) if i != s],
+            [(sl, dist(sl[2], left_end)) for sl in slices],
             key=itemgetter(1))
+
+        # right slices sorted by distance
         right_slices = sorted(
-            [(i, dist(this_slice[1], slice_lums[i][0]))
-                for i in range(len(slice_lums)) if i != s],
+            [(sl, dist(right_end, sl[1])) for sl in slices],
             key=itemgetter(1))
 
-        left = left_slices[0]
-        right = right_slices[0]
-        if left[0] == right[0]:
-            if left[1] < right[1]:
-                right = right_slices[1]
-            else:
-                left = left_slices[1]
-        lefts[(right[0],s)] = (s, left[0])
-        rights[(left[0],s)] = (s, right[0])
-
-    for k, v in lefts.items():
-        if v not in lefts:
-            print k, v
-            left_end = v[0]
-            lefts.pop(k)
-            break
-
-    for k in rights.keys():
-        if k[0] == left_end:
-            left_start = k
-            break
-
-    ordering = [left_end]
-    while True:
-        ordering += [left_start[1]]
-        if left_start not in rights:
-            break
+        # Pick the best addition, left or right
+        if left_slices[0][1] < right_slices[0][1]:
+            left_slice = left_slices[0][0]
+            ordering.insert(0, left_slice[0])
+            left_end = left_slice[1]
+            slices.remove(left_slice)
         else:
-            left_start = rights[left_start]
-    ordering.pop()
+            right_slice = right_slices[0][0]
+            ordering.append(right_slice[0])
+            right_end = right_slice[2]
+            slices.remove(right_slice)
 
-    unshredded = Image.new("RGBA", im.size)
-    for i, slice in enumerate(ordering):
-        x1, y1 = 32*slice, 0
-        x2, y2 = x1 + 32, h
-        slice_im = im.crop((x1, y1, x2, y2))
-        unshredded.paste(slice_im, (i*32, 0))
+    unshred(im, ordering)
 
-    unshredded.show()
